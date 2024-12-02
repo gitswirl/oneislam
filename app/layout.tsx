0aeffa4b16613816a3dcfa12e6c786c9bf7d8c0e
haseeb arshad <haseebarshad821@gmail.com>
2024-12-02 13:27:26 +0500

    1

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// app/layout.tsx
+"use client";
+import React from "react";
+import "./globals.scss";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+// components/home/page.ts
+"use client";
+import React from "react";
+const Home: React.FC = () => {
+  return <p>one-islam is comming soon.</p>;
+};
+export default Home;
